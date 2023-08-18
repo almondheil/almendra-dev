070734628ae93d451d8f9280ae958e7f76d01298
Almond Heil <almond@almendra.dev>
2023-08-18 15:02:59 -0600

    Update utils to use datetime and be more accurate
    For the "-Present" output, it now more intelligently checks the day of
    the end date. (e.g if an event ends 2023-08-18, it will show as
    "-Present" until midnight when it becomes 2023-08-19

--- utils.py
+++ utils.py
@@ -1,111 +1,60 @@
 from datetime import datetime
 
 """
-Convert from the number of a month to a three-letter abbreviation for that
-month. If month is not in range (1, 12), will return "Err."
-"""
-def num2month(num):
-    if num == 1:
-        return "Jan"
-    elif num == 2:
-        return "Feb"
-    elif num == 3:
-        return "Mar"
-    elif num == 4:
-        return "Apr"
-    elif num == 5:
-        return "May"
-    elif num == 6:
-        return "Jun"
-    elif num == 7:
-        return "Jul"
-    elif num == 8:
-        return "Aug"
-    elif num == 9:
-        return "Sep"
-    elif num == 10:
-        return "Oct"
-    elif num == 11:
-        return "Nov"
-    elif num == 12:
-        return "Dec"
-    else:
-        return "Err"
-
-"""
 Convert from a string start_date and end_date to HTML code that describes the
 appropriate time range.
 
-Dates are assumed to be formatted "YYYY-mm-dd" as they are in sqlite3, but
-days will be ignored when converting.
+Dates are assumed to be formatted "YYYY-mm-dd" as they are in sqlite3.
 
-Examples.
-    in:
-        2023-01-01, 2023-03-01
-    out:
-        <time datetime="2023-01">Jan</time>-<time datetime="2023-03">Mar 2023</time>
-    in:
-        2023-01-01, 2023-01-01
-    out:
-        <time datetime="2023-01">Jan 2023</time>
-    in:
-        2022-10-01, 2023-02-01
-    out:
-        <time datetime="2022-10">Oct 2022</time>-<time datetime="2023-02">Feb 2023</time>
-    in:
-        2022-10-01, 2100-10-01
-    out:
-        <time datetime="2022-10">Oct 2022</time>-<time datetime="present">Present</time>
+args:
+    start_datestr: string representation of start time, YYYY-mm-dd
+    end_datestr: string representation of end time, YYYY-mm-dd
+returns:
+    valid HTML describing the time range provided (see below)
 
+output possibilities
+    MMM is the abbreviation of the current month %b (Jan-Dec)
+    YYYY is simply the full year
+
+    End date today or in the future:
+        MMM YYYY-Present
+
+    Same month of the same year:
+        MMM YYYY
+
+    Different months of the same year:
+        MMM-MMM YYYY
+
+    Different months of different years:
+        MMM YYYY-MMM YYYY
 """
-def dates2range(start_date, end_date):
-    # Parse start and end times
-    (s_year, s_month, _) = start_date.split('-')
-    (e_year, e_month, _) = end_date.split('-')
-
-    # Integer versions of these vars
-    s_year_int  = int(s_year)
-    e_year_int  = int(e_year)
-    s_month_int = int(s_month)
-    e_month_int = int(e_month)
+def dates2range(start_datestr: str, end_datestr: str) -> str:
+    # Store start and end as the provided values, but add a time of 23:59:59 to ensure
+    # that (now <= end) will be true if today is the day of end
+    start = datetime.strptime(start_datestr + " 23:59:59", "%Y-%m-%d %H:%M:%S").date()
+    end = datetime.strptime(end_datestr + " 23:59:59", "%Y-%m-%d %H:%M:%S").date()
+    now = datetime.now().date()
 
     # String versions of the months involved
-    s_month_name = num2month(s_month_int)
-    e_month_name = num2month(e_month_int)
+    start_month_name = datetime.strftime(start, '%b')
+    end_month_name = datetime.strftime(end, '%b')
 
-    # Ends later than now, return Aaa YYYY-Present
-    curr_month = datetime.now().month
-    curr_year = datetime.now().year
-    if (curr_year <= e_year_int and curr_month <= e_month_int):
+    if (now <= end): 
         result = '<time datetime="{0}-{1}">{2} {0}</time>-<time datetime="present">Present</time>'
-        return result.format(s_year, s_month, s_month_name)
+        return result.format(start.year, start.month, start_month_name)
 
+    elif (start.year != end.year):
+        result = '<time datetime="{0}-{1}">{2} {0}</time>-<time datetime="{3}-{4}">{5} {3}</time>'.format(
+            start.year, start.month, start_month_name,
+            end.year, end.month, end_month_name)
 
-    # Two different years, return Aaa YYY1-BBB YYY2
-    if (s_year != e_year):
-        result = '<time datetime="{0}-{1}">{2} {0}</time>-<time datetime="{3}-{4}">{5} {3}</time>'
-        return result.format(s_year, s_month, s_month_name,
-                             e_year, e_month, e_month_name)
+    elif (start.month == end.month):
+        result = '<time datetime="{0}-{1}">{2} {0}</time>'.format(
+            start.year, start.month, start_month_name)
 
-    # Same year same month, return Aaa YYYY
-    if (s_month == e_month):
-        result = '<time datetime="{0}-{1}">{2} {0}</time>'
-        return result.format(s_year, s_month, s_month_name)
+    else:
+        result = '<time datetime="{0}-{1}">{2}</time>-<time datetime="{3}-{4}">{5} {3}</time>'.format(
+            start.year, start.month, start_month_name,
+            end.year, end.month, end_month_name)
 
-    # Same year different months, return Aaa-Bbb YYYY
-    result = '<time datetime="{0}-{1}">{2}</time>-<time datetime="{3}-{4}">{5} {3}</time>'
-    return result.format(s_year, s_month, s_month_name,
-                         e_year, e_month, e_month_name)
-
-
-"""
-Dates which test each case, left here because who cares
-"""
-if __name__ == '__main__':
-    print(dates2range("2023-01-01", "2023-03-01"))
-    print(dates2range("2023-01-01", "2023-01-01"))
-    print(dates2range("2022-10-01", "2023-02-01"))
-    # If this is still going in 2100 I may have done something wrong
-    # This should trigger the "Present" case
-    print(dates2range("2022-10-01", "2100-10-01"))
-    
+    return result
